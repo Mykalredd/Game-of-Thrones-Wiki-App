b3edaa9cfd2f374776f42feb5dbc009789f9bb56
Mykalredd <126212718+Mykalredd@users.noreply.github.com>
2023-11-30 09:15:51 -0800

    Added notes, adjust img files, redeploy gh pages.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import axios from 'axios';
-import styles from './CharacterCard.module.css';
+import React, { useState, useEffect } from 'react'; /* Hooks used to manage state and handle effects*/
+import { useNavigate } from 'react-router-dom'; /*Navigation Hook*/
+import axios from 'axios'; /*Used for making my HTTP request*/
+import styles from './CharacterCard.module.css'; /* Used to make my styling independent*/
 
 const CharacterCard = ({ character }) => {
   const [allegianceNames, setAllegianceNames] = useState([]);
   const navigate = useNavigate(); 
 
+  /*Functional Component w/ prop. 
+  Used hook to create state variable 
+  and function to update it in empty array.
+  Hook for my navi function to my routes. */
+  
+  
   useEffect(() => {
     const fetchAllegianceNames = async () => {
       try {
@@ -25,19 +31,30 @@ const CharacterCard = ({ character }) => {
     }
   }, [character.allegiances]);
 
+  /* Hook for component effects, 
+  fetches allegiance names when the character.allegiances prop changes. 
+  asynchronous function "fetchAllegianceNames" 
+  uses Axios for requests for allegiance data using character.allegiances array.
+  Names are stored in the allegianceNames state using setAllegianceNames.
+  If data in the "character" prop, run "fetchAllegianceNames" function. */
+
   const navigateToCharacterDetail = () => {
     navigate(`/characters/${character.url.replace(/[^0-9]/g, "")}`);
   };
 
+  /* Function that uses navigate function to go to character page based on the 
+  character.url prop and removes non-numeric characters from the URL. */
+  
+  
   return (
     <div className={styles.CharacterCard} onClick={navigateToCharacterDetail}>
       <h2>{character.name || character.aliases[0]}</h2>
       <img
         src={
           process.env.PUBLIC_URL +
-          (character.gender === 'Male' ? '/male-icon.png' : '/female-icon.png')
+          (character.gender === 'Male' ? '/maleImg.png' : '/femaleImg.png')
         }
-        alt="Gender Icon"
+        alt="Gender Img"
       />
       <p>Aliases: {character.aliases.join(', ') || 'Unknown'}</p>
       <p>Gender: {character.gender || 'Unknown'}</p>
@@ -48,5 +65,11 @@ const CharacterCard = ({ character }) => {
     </div>
   );
 };
+
+/*Defines the structure and content of my CharacterCard component.
+Uses the CSS class styles.CharacterCard for styling and runs the 
+navigateToCharacterDetail function when clicked.
+Info from the character prop is displayed, and conditional rendering 
+is used for the gender img based on character's gender. */
 
 export default CharacterCard;
