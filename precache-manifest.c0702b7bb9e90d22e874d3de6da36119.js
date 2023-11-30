4e58931508b360ddf1cbf20b0686da4910068ddc
Mykalredd <126212718+Mykalredd@users.noreply.github.com>
2023-11-30 09:27:19 -0800

    Updates

---
+++ precache-manifest.c0702b7bb9e90d22e874d3de6da36119.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f7e4a5215b41bf0bec47be4f7e11b5",
+    "url": "/Game-of-Thrones-Wiki-App/index.html"
+  },
+  {
+    "revision": "6075175b19264695e02a",
+    "url": "/Game-of-Thrones-Wiki-App/static/css/main.5614c3a8.chunk.css"
+  },
+  {
+    "revision": "590da1388bd0f1b63a0e",
+    "url": "/Game-of-Thrones-Wiki-App/static/js/2.7b0a39d9.chunk.js"
+  },
+  {
+    "revision": "8ba8997f9f8a1b4cb4c9278bd845eca8",
+    "url": "/Game-of-Thrones-Wiki-App/static/js/2.7b0a39d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6075175b19264695e02a",
+    "url": "/Game-of-Thrones-Wiki-App/static/js/main.f4cd61c7.chunk.js"
+  },
+  {
+    "revision": "99fceaef5d6e7e093ada",
+    "url": "/Game-of-Thrones-Wiki-App/static/js/runtime-main.91ede704.js"
+  },
+  {
+    "revision": "889acd69f50383d55967b4096ab15c72",
+    "url": "/Game-of-Thrones-Wiki-App/static/media/GoTHDImage.889acd69.jpeg"
+  },
+  {
+    "revision": "28d55787646c601e1f11f10eed35aa56",
+    "url": "/Game-of-Thrones-Wiki-App/static/media/GoTThemeSong.28d55787.mp3"
+  }
+]);
